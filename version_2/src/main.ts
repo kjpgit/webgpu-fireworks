eeb71f90aa76d129f1e5247d51a8cd33cd481f15
kjpgit <kjpgit@users.noreply.github.com>
2023-12-23 19:01:19 -0600

    s

--- version_2/src/main.ts
+++ version_2/src/main.ts
@@ -1,12 +1,101 @@
-import { BufferWrapper } from "./buffer.js";
-import { Scene } from "./fireworks.js";
+import { BufferWrapper } from "./buffer.js"
+import { Scene } from "./fireworks.js"
 
 
-function do_throw(errorMessage: string): never {
-    throw new Error(errorMessage);
+export function do_throw(errorMessage: string): never {
+    throw new Error(errorMessage)
 }
 
-const init = async () => {
+
+class Main
+{
+    is_fullscreen = false
+    last_time = 0
+    pause_time = 0
+    pause_total = 0
+    stats_time_start = 0
+    num_frames = 0
+    scene: Scene
+
+    constructor() {
+        this.scene = new Scene()
+
+        addEventListener("dblclick", e => this.on_double_click(e))
+        addEventListener("keydown", e => this.on_keydown(e))
+        addEventListener("resize", e => this.on_resize(e))
+    }
+
+    on_resize(event: Event) {
+        console.log("resized")
+    }
+
+    on_keydown(e: KeyboardEvent) {
+        console.log(`You pressed ${e.key}`)
+        if (e.key == "f") {
+            this.toggleFullScreen()
+        }
+        if (e.key == " ") {
+            if (this.pause_time == 0) {
+                this.pause_time = this.last_time
+            } else {
+                this.pause_total += this.last_time - this.pause_time
+                this.pause_time = 0
+            }
+        }
+        if (e.key == "j") {
+            if (this.pause_time != 0) {
+                this.pause_time -= 1/60
+            }
+        }
+        if (e.key == "k") {
+            if (this.pause_time != 0) {
+                this.pause_time += 1/60
+            }
+        }
+    }
+
+    on_double_click(event: Event) {
+        console.log("doubleclick")
+        this.toggleFullScreen()
+    }
+
+
+    toggleFullScreen() {
+        if (this.is_fullscreen) {
+            this.closeFullscreen()
+            this.is_fullscreen = false
+        } else {
+            this.openFullscreen()
+            this.is_fullscreen = true
+        }
+    }
+
+    openFullscreen() {
+        let elem: any = document.documentElement
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen()
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen()
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen()
+        }
+    }
+
+    closeFullscreen() {
+        let elem: any = document
+        if (elem.exitFullscreen) {
+            elem.exitFullscreen()
+        } else if (elem.webkitExitFullscreen) { /* Safari */
+            elem.webkitExitFullscreen()
+        } else if (elem.msExitFullscreen) { /* IE11 */
+            elem.msExitFullscreen()
+        }
+    }
+
+}
+
+
+const init_webgpu = async (main: Main) => {
     // Initialize - make sure we can initialize webgpu
     if (!navigator.gpu) {
         do_throw("WebGPU cannot be initialized - navigator.gpu not found");
@@ -134,21 +223,17 @@ const init = async () => {
     let renderTarget: GPUTexture | undefined = undefined;
     let renderTargetView: GPUTextureView;
 
-    const scene = new Scene();
-    var stats_time_start = 0
-    var num_frames = 0
-
-    async function frame(elapsedMs: DOMHighResTimeStamp) {
+    async function frame(elapsedMs: DOMHighResTimeStamp, main: Main) {
         // This isn't perfectly accurate (off by 1?), averaging the last 60
         // frametimes might be more precise.
         const elapsed_secs = elapsedMs / 1000
-        if (elapsed_secs - stats_time_start > 1) {
-            console.log(`fps: ${num_frames}`);
-            num_frames = 0
-            stats_time_start = elapsed_secs
+        if (elapsed_secs - main.stats_time_start > 1) {
+            console.log(`fps: ${main.num_frames}`);
+            main.num_frames = 0
+            main.stats_time_start = elapsed_secs
         }
-        num_frames += 1
-        g_last_time = elapsed_secs
+        main.num_frames += 1
+        main.last_time = elapsed_secs
 
         const currentWidth = canvas.clientWidth * devicePixelRatio;
         const currentHeight = canvas.clientHeight * devicePixelRatio;
@@ -182,9 +267,9 @@ const init = async () => {
         await vertexBufferCPU.mapAsync(GPUMapMode.WRITE);
         const cpu_buffer = new Float32Array(vertexBufferCPU.getMappedRange());
         const cpu_buffer_wrapper = new BufferWrapper(cpu_buffer);
-        scene.set_screen_size(canvas.width, canvas.height)
-        const scene_time = (g_pause_time == 0 ? elapsed_secs : g_pause_time) - g_pause_total;
-        scene.draw(cpu_buffer_wrapper, scene_time);
+        const scene_time = (main.pause_time == 0 ? elapsed_secs : main.pause_time) - main.pause_total;
+        main.scene.set_screen_size(canvas.width, canvas.height)
+        main.scene.draw(cpu_buffer_wrapper, scene_time);
         const cpu_buffer_bytes_used = cpu_buffer_wrapper.bytes_used();
         vertexBufferCPU.unmap();
 
@@ -211,93 +296,19 @@ const init = async () => {
         passEncoder.end();
 
         device.queue.submit([commandEncoder.finish()]);
-        requestAnimationFrame(frame);
+        requestAnimationFrame((elapsedMs) => frame(elapsedMs, main));
     }
 
-    requestAnimationFrame(frame);
-};
-
-const try_init = async () => {
-    try {
-        await init();
-    } catch (err: any) {
-        const errDiv = document.getElementById("error-message")!
-        errDiv.classList.remove('hidden')
-        errDiv.innerHTML = err.toString()
-        throw err;
-    }
-};
-
-try_init();
-
-addEventListener("resize", (event) => {
-    console.log("resized")
-});
-
-addEventListener('keydown', function (e) {
-    console.log(`You pressed ${e.key}`);
-    if (e.key == "f") {
-        toggleFullScreen()
-    }
-    if (e.key == " ") {
-        if (g_pause_time == 0) {
-            g_pause_time = g_last_time;
-        } else {
-            g_pause_total += g_last_time - g_pause_time
-            g_pause_time = 0;
-        }
-    }
-    if (e.key == "j") {
-        if (g_pause_time != 0) {
-            g_pause_time -= 1/60;
-        }
-    }
-    if (e.key == "k") {
-        if (g_pause_time != 0) {
-            g_pause_time += 1/60;
-        }
-    }
-}, false);
-
-addEventListener("dblclick", (event) => {
-    console.log("doubleclick")
-    toggleFullScreen()
-});
-
-
-var is_fullscreen = false;
-var g_last_time = 0;
-var g_pause_time = 0;
-var g_pause_total = 0;
-
-function toggleFullScreen() {
-    if (is_fullscreen) {
-        closeFullscreen();
-        is_fullscreen = false;
-    } else {
-        openFullscreen();
-        is_fullscreen = true;
-    }
+    requestAnimationFrame((elapsedMs) => frame(elapsedMs, main));
 }
 
-function openFullscreen() {
-    let elem: any = document.documentElement;
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-}
-
-function closeFullscreen() {
-    let elem: any = document;
-    if (elem.exitFullscreen) {
-        elem.exitFullscreen();
-    } else if (elem.webkitExitFullscreen) { /* Safari */
-        elem.webkitExitFullscreen();
-    } else if (elem.msExitFullscreen) { /* IE11 */
-        elem.msExitFullscreen();
-    }
+var main
+try {
+    main = new Main()
+    await init_webgpu(main)
+} catch (err: any) {
+    const errDiv = document.getElementById("error-message")!
+    errDiv.classList.remove('hidden')
+    errDiv.innerHTML = err.toString()
+    throw err
 }
