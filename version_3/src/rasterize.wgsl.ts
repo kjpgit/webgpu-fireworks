4664ecd960a1143140b1336d2f6b9aaadf5610b2
kjpgit <kjpgit@users.noreply.github.com>
2024-01-01 19:50:40 -0600

    try 1

--- version_3/src/rasterize.wgsl.ts
+++ version_3/src/rasterize.wgsl.ts
@@ -7,7 +7,6 @@ export var RasterizeCode = `
 ${constants.WGSL_INCLUDE}
 
 
-//@group(0) @binding(0) var<uniform>            g_uniform: UniformData;
 @group(0) @binding(1) var<storage, read>        g_misc: MiscDataRead;
 @group(0) @binding(2) var<storage, read>        g_fine_shapes: array<FineShape>;
 @group(0) @binding(3) var<storage, read_write>  g_color_buffer: array<vec3<f32>>;
@@ -39,40 +38,30 @@ const PERFORMANCE_TEST_MONOCHROME = false;
 
 
 //
-// Rasterize fine shapes, writing to atomic texture memory.
+// Rasterize fine shapes, writing to texture memory.
 //
-// Each workgroup is assigned a rectangular section of screen pixels,
-// and fully processes it.
 //
-// Each thread in the workgroup processes a portion of the pixel area.
-//
-const WG_THREADS_X = 16;
-const WG_THREADS_Y = 16;
-const TILE_SUBDIVISIONS = 14;
+const WG_RASTER_WORKLOAD = 256;
 
-//var<workgroup> private_color_storage: array<vec3<f32>, WG_RASTER_PIXELS_X*WG_RASTER_PIXELS_Y>;
 //var<workgroup> shape: FineShape;
 
-@compute @workgroup_size(WG_THREADS_X, WG_THREADS_Y)
+@compute @workgroup_size(16, 16)
 fn fine_main(
     @builtin(workgroup_id) workgroup_id : vec3<u32>,
     @builtin(local_invocation_id) local_invocation_id : vec3<u32>,
 )
 {
-    // We process one logical tile out of the 8x8 grid
-    let tile_x = workgroup_id.x;
-    let tile_y = workgroup_id.y;
+    // Each WG processes 1 tile, which contains 256 pixels (1 pixel per thread)
+    // Dispatch will be 4480 WGs
 
-    // Since one tile is 224*80=17920 px, we divide it further into 16x16 for each WG.
-    // TODO: try looping instead
-    let offset_x = (workgroup_id.z % TILE_SUBDIVISIONS) * 16;
-    let offset_y = (workgroup_id.z / TILE_SUBDIVISIONS) * 16;
-
+    let my_tile_id = i32(workgroup_id.x);
+    let x = i32(my_tile_id % NUM_TILES_X);
+    let y = i32(my_tile_id / NUM_TILES_X);
 
     // The view box this *workgroup* is responsible for.
     let wg_view_min = vec2<f32>(
-        f32(workgroup_id.x * WG_RASTER_PIXELS_X + offset_x),
-        f32(workgroup_id.y * WG_RASTER_PIXELS_Y + offset_y),
+        f32(x * TILE_SIZE_X),
+        f32(y * TILE_SIZE_Y),
     );
 
     // The view box this *thread* is responsible for.
@@ -100,25 +89,13 @@ fn fine_main(
     var final_color = clear_color;
 
     if (!PERFORMANCE_TEST_NOOOP) {
-        // bitmap index scan
-        let total_shapes = i32(g_misc.num_fine_shapes_per_row[tile_y]);
-
-        let shape_mask = (1u << (24 + tile_x));
-        for (var s = 0; s < total_shapes; s++) {
-            let shape_idx = g_fine_shapes_index[tile_y][s];
-            if ((shape_idx & shape_mask) == 0) {
-                continue;
-            }
-            let shape = g_fine_shapes[shape_idx & 0xffffff];
-            //shape = g_fine_shapes[shape_idx & 0xffffff];
+        // hashtable scan
+        let ptr_offset = g_misc.tile_shape_index[y][x].offset;
+        let num_ptrs = g_misc.tile_shape_index[y][x].length;
+        for (var s = 0; s < num_ptrs; s++) {
+            let shape_idx = g_misc.tile_shape_pointers[ptr_offset + s];
+            let shape = g_fine_shapes[shape_idx];
             //workgroupBarrier();  // Great way to prove we have uniform control flow!
-
-        /*
-        // dumb full array scan
-        let total_shapes = i32(g_misc.num_fine_shapes);
-        for (var s = 0; s < total_shapes; s++) {
-            let shape = g_fine_shapes[s];
-        */
 
             let shape_size = shape.view_size_x;
             let shape_vpos = shape.view_position;
@@ -142,7 +119,7 @@ fn fine_main(
                             if (PERFORMANCE_TEST_MONOCHROME) {
                                 final_color.b = 1.0;
                             } else {
-                                final_color += get_shape_color(shape).rgb * ratio;
+                                final_color += shape.color * ratio;
                             }
                         }
                     }
@@ -163,24 +140,6 @@ fn output_texture_store(view_x: f32, view_y: f32, color: vec3<f32>)
 }
 
 
-
-
-
-// Workgroup fast memory
-//
-/*
-fn output_texture_add_private(view_x: f32, view_y: f32, color: vec3<f32>)
-{
-    let private_linear = u32(floor(view_x) + floor(view_y) * WG_RASTER_PIXELS_X);
-    private_color_storage[private_linear] += color;
-}
-
-fn output_texture_get_private(view_x: f32, view_y: f32) -> vec3<f32>
-{
-    let private_linear = u32(floor(view_x) + floor(view_y) * WG_RASTER_PIXELS_X);
-    return private_color_storage[private_linear];
-}
-*/
 
 /*
 // From stackoverflow
